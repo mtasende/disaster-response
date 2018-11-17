ba85d9cb37a102760554741471a675dac93af0a6
mtasende <miguel.tasende@gmail.com>
2018-11-17 13:44:07 -0300

    chore: setting up the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='disaster-response',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Disaster response pipeline',
+    author='Miguel Tasende',
+    license='MIT',
+)
